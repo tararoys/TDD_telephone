88d932e8d489200559c2c82725e118435b623965
Christopher Scherer <christopherscherer@Christophers-MacBook-Pro-3.local>
2014-03-11 10:59:08 -0500

    second test

--- tests_to_do_list.rb
+++ tests_to_do_list.rb
@@ -2,5 +2,9 @@ require 'rspec'
 require_relative 'todo'
 
 describe ToDoList do
-
+  context "When a ToDoList is created"
+    it "should have a title" do
+      list = ToDoList.new(title: "Tara's to dos'")
+      expect(list.title).to_not eq(nil)
+    end
 end
