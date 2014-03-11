3a5669e121d6ed6c42c6b90107bd863c1f944354
Christopher Scherer <christopherscherer@Christophers-MacBook-Pro-3.local>
2014-03-11 11:20:36 -0500

    added description test

--- tests_to_do_list.rb
+++ tests_to_do_list.rb
@@ -7,4 +7,9 @@ describe ToDoList do
       list = ToDoList.new(title: "Tara's to dos'")
       expect(list.title).to_not eq(nil)
     end
+
+    it "should have a description" do
+      list = ToDoList.new(description: "A list of things for Tara to do")
+      expect(list.description).to_not eq(nil)
+    end
 end
